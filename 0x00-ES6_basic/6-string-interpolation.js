d33cb2f16f4baeb30555e9023a8f295215c5e3b9
victorpropython <victorchibuzor3109@gmail.com>
2024-10-07 14:04:23 +0100

    fot task 6

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -1,11 +1,12 @@
 export default function getSanFranciscoDescription() {
-    const year = 2017;
-    const budget = {
-        income: '$119,868',
-        gdp: '$154.2 billion',
-        capital: '4178.479',
-    };
-    return `As of ${year}, it was the seventh highest income county in the United States,
-             With a per capita personal income of ${budget.income}. As of 2015, Sanfrancisco
-            Proper had a GDP of ${budget.gdp}, and a GDp per capital of ${budget.capita}.`;
+  const year = 2017;
+  const budget = {
+    income: '$119,868',
+    gdp: '$154.2 billion',
+    capital: '4178.479',
+};
+
+  return `As of ${year}, it was the seventh highest income county in the United States,
+        With a per capita personal income of ${budget.income}. As of 2015, Sanfrancisco
+          Proper had a GDP of ${budget.gdp}, and a GDp per capital of ${budget.capita}.`;
 }
