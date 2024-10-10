1c9492e41ceaba40d5e30f7c0fc7f06293926dcd
victorpropython <victorchibuzor3109@gmail.com>
2024-10-10 03:31:44 +0100

    for task 7

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -11,7 +11,24 @@ export default class Airport {
       throw new TypeError('Code must be a string');
     }
   }
-  toString() {
+
+  get name() {
+    return (this._name);
+  }
+
+  set name(value) {
+    this._name = name;
+  }
+
+  get code() {
+    return (this._code);
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  get [Symbol.toStringTag]() {
     return `${this._name} ${this._code}`;
   }
 }
