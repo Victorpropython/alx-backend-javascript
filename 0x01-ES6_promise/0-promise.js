bc466f39df9cc42ebe3df397415485b2416ed0d2
victorpropython <victorchibuzor3109@gmail.com>
2024-10-20 15:54:53 +0100

    for tas 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,15 +1,5 @@
 /*Function to return a promise using a function prototype
  */
 function getResponseFromAPI() {
-	return new Promise((reject, resolv) => {
-		const success = true;
-
-		setTimeout(() => {
-			if (success){
-				resolve("Was successful");
-			}else {
-			reject("Was unsuccessful");
-			}
-		}, 2000):
-	});
+	return new Promise((reject, resolve));
 }
