bd6b386cd20d982effd632e9435364dcc6822cce
victorpropython <victorchibuzor3109@gmail.com>
2025-01-14 06:49:38 +0100

    for task 6

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -92,7 +92,8 @@ const SERVER_ROUTE_HANDLERS = [
         .catch((err) => {
           if (err.message === 'Cannot load the database'){
             res.statusCode = 404;
-          }else {
+          }
+          else {
             res.statusCode = 200;
           }
           responseParts.push(err instanceof Error ? err.message : err.toString());
--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -8,7 +8,7 @@ app.get('/', (_, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log('server listening on PORT ${PORT}');
+  console.log(`server listening on PORT ${PORT}`);
 });
 
 module.exports = app;
