f0032256ef30451e6f575009f361403eac19ec06
victorpropython <victorchibuzor3109@gmail.com>
2025-01-17 04:03:43 +0100

    for task 0

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -3,7 +3,7 @@
  * @param{a , b} being the argument and rounding it up as a sum
  */
 function calculateNumber(a, b) {
-  return Math.round(a) + Math.ceil(b);
+  return Math.round(a) + Math.round(b);
 }
 
 module.exports = calculateNumber;
