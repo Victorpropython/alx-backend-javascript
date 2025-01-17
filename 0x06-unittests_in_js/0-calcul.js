4125610aa0ee517e1d45eeca7174558dd991aa70
victorpropython <victorchibuzor3109@gmail.com>
2025-01-17 03:38:35 +0100

    for task 0

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,9 @@
+/**
+ * function to calculate the sum of two numbers
+ * @param{a , b} being the argument and rounding it up as a sum
+ */
+function calculateNumber(a, b) {
+  return Math.round(a) + Math.round(b);
+}
+
+module.exports = calculateNumber
